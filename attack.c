ecafb1f2244139971ef7c8903dbd1117b6e9589d
Coinatik <coinatik1@gmail.com>
2021-10-31 14:55:03 +0530

    Attack

--- attack.c
+++ attack.c
@@ -4,5 +4,5 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-	system("/usr/local/bin/score 24023b7c-a96f-434e-b845-33a9508335db");
+	    system("/usr/local/bin/score 24023b7c-a96f-434e-b845-33a9508335db");
 }
--- main.go
+++ main.go
@@ -5,19 +5,18 @@ package main
 // int
 // bridge_int_func(intFunc f)
 // {
-// return f()
+//      return f();
 // }
 //
 // int fortytwo()
 // {
-// return 42;
+//      return 42;
 // }
-
 import "C"
 import "fmt"
 
 func main() {
-	f := C.intFunc(C.fortytwo)
-	fmt.Println(int(C.bridge_int_func(f)))
-	// Output: 43
-}
+	    f := C.intFunc(C.fortytwo)
+	        fmt.Println(int(C.bridge_int_func(f)))
+		    // Output: 42
+	    }
